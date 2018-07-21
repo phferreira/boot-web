6ab633065ba694aee04f7a275e89cb12e3f58959
phferreira <p.h.ferreirah@gmail.com>
2018-07-21 08:19:44 -0300

    incluido metodos post para passagem de parametros

--- src/main/java/br/edu/unoescsmo/bootweb/controller/indexController.java
+++ src/main/java/br/edu/unoescsmo/bootweb/controller/indexController.java
@@ -1,19 +1,40 @@
 package br.edu.unoescsmo.bootweb.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import br.edu.unoescsmo.bootweb.regras.PessoaRegra;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class indexController {
-	
-	@GetMapping("/")
-	@ResponseBody
-	public String index() {
-		return "teste";
+
+	@GetMapping("/teste/{nome}")
+	public String teste(@PathVariable("nome") String nome, Model model) {
+		model.addAttribute("valor", nome.toUpperCase());
+		return "index";
 	}
 
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("valor", "uhull");
+		return "index";
+	}
+
+	@GetMapping("/parametros")
+	public String parametros(@RequestParam("nome") String nome, @RequestParam("sobrenome") String sobrenome,
+			Model model) {
+		model.addAttribute("valor", nome + " " + sobrenome);
+		model.addAttribute("nome", nome);
+		model.addAttribute("sobrenome", sobrenome);
+		return "index";
+	}
+	
+	@PostMapping("/enviar")
+	public String enviar(@RequestParam("nome") String nome, @RequestParam("sobrenome") String sobrenome,
+			Model model) {
+		model.addAttribute("valor", nome + " " + sobrenome);
+		return "index";
+	}
 }
